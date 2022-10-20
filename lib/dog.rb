8a2350bc6a261c01668afe0bcfec7627f7ca4695
oumarichard <richard.ouma@student.moringaschool.com>
2022-10-20 22:22:31 +0300

    Tests passed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
+def name=(dog_name)
+  @name = dog_name
+end
 
+def name
+  @name
+end
+end
+
+teddy = Dog.new
+teddy.name = "Teddy"
+
+class Dog
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+
+  def breed
+    @breed
+  end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,28 @@
 class Person
+ def name=(name_of_person)
+@name = name_of_person
+end
 
+def name
+@name_of_person
+end
+end
+
+# boy = Person.new
+# boy.name = "Richie"
+
+class Person
+  def name
+    @name
+  end
+end
+
+class Person
+  def job=(name_of_job)
+    @job = name_of_job
+  end
+
+  def job
+    @job
+  end
 end
